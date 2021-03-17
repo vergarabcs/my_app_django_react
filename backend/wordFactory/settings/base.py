3f975ca855967bb8aacda3aac5962738f3e794a6
Bill Carlo Vergara <b.vergara>
2021-03-18 06:09:15 +0800

    chat server tutorial is running. Tutorial made me run redis

--- backend/wordFactory/settings/base.py
+++ backend/wordFactory/settings/base.py
@@ -26,6 +26,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "exampleapp.apps.ExampleappConfig",
+    "chat.apps.ChatConfig",
+    
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
--- backend/wordFactory/urls.py
+++ backend/wordFactory/urls.py
@@ -7,8 +7,9 @@ import django_js_reverse.views
 
 
 urlpatterns = [
-    path("", lambda request : redirect("/exampleapp/")),
+    path("", lambda request : redirect("/chat/")),
     path("admin/", admin.site.urls, name="admin"),
     path("jsreverse/", django_js_reverse.views.urls_js, name="js_reverse"),
     path("exampleapp/", include("exampleapp.urls"), name="exampleapp"),
+    path("chat/", include('chat.urls')),
 ]
