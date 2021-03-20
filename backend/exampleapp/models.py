5eaa53bea18c6b69633b5a32687fc065da81b29f
Bill Carlo Vergara <b.vergara>
2021-03-20 14:07:21 +0800

    update requirements

--- backend/exampleapp/models.py
+++ backend/exampleapp/models.py
@@ -26,3 +26,4 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
--- backend/wordFactory/settings/base.py
+++ backend/wordFactory/settings/base.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "common",
     "users",
     "channels",
+    "rest_framework"
 ]
 
 MIDDLEWARE = [
